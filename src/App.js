6d9ea775c284760dc0afbbe7c60204c376a3da42
Jeongyong Park <jypark.inno@gmail.com>
2021-02-09 10:17:22 +0900

    eslint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import BoreholeDataGrid from './components/BoreholeDataGrid';
+import "./App.css";
+import BoreholeDataGrid from "./components/BoreholeDataGrid";
 
 function App() {
   return (
